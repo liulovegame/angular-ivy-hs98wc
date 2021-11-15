db2d1b952e50f345b185fc86cd7ab5e2f42660d3
liulovegame <291908992@qq.com>
2021-11-15 15:45:40 +0800

    3 files modified

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Route } from '@angular/router';
 import { HelloComponent } from './hello.component';
 import { BasisComponent } from './pages/basis/basis.component';
+import { ParentComponent } from './pages/parent/parent.component';
 import { ParentModule } from './pages/parent/parent.module';
 
 type IRoutes = Route & {
@@ -20,10 +21,15 @@ export const routes: IRoutes[] = [
     path: 'basis',
     component: BasisComponent,
   },
+  {
+    path: 'parent',
+    title: '父子传值，事件传参',
+    component: ParentComponent,
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), ParentModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRouterModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRouterModule } from './app-router.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRouterModule],
+  imports: [BrowserModule, AppRouterModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
