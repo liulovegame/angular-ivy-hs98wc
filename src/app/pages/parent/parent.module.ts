a80cfa6feed58be98d6dadf18acd12c698589411
liulovegame <291908992@qq.com>
2021-11-15 17:07:47 +0800

    2 files modified

--- src/app/pages/parent/parent.module.ts
+++ src/app/pages/parent/parent.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ChildTmpComponent } from './child-tmp/child-tmp.component';
@@ -5,7 +6,7 @@ import { ChildComponent } from './child/child.component';
 import { ParentComponent } from './parent.component';
 
 @NgModule({
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   declarations: [ParentComponent, ChildComponent, ChildTmpComponent],
   exports: [ParentComponent],
 })
